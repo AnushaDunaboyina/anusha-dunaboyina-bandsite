a72685d302c8fb22dd9a98c21d532b143ea61092
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-02-01 23:25:02 -0500

    Made few changes in shows js and added stylings for shows page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -20,22 +20,22 @@ document.addEventListener('DOMContentLoaded', () => {
         headingsRowDiv.className = 'show-headings';
 
         const dateHeadingDiv = document.createElement('div');
-        dateHeadingDiv.className = 'show-heading__date';
+        dateHeadingDiv.className = 'show-headings__date';
         dateHeadingDiv.textContent = 'DATE';
         headingsRowDiv.appendChild(dateHeadingDiv);
 
         const venueHeadingDiv = document.createElement('div');
-        venueHeadingDiv.className = 'show-heading__venue';
+        venueHeadingDiv.className = 'show-headings__venue';
         venueHeadingDiv.textContent = 'VENUE';
         headingsRowDiv.appendChild(venueHeadingDiv);
 
         const locationHeadingDiv = document.createElement('div');
-        locationHeadingDiv.className = 'show-heading__location';
+        locationHeadingDiv.className = 'show-headings__location';
         locationHeadingDiv.textContent = 'LOCATION';
         headingsRowDiv.appendChild(locationHeadingDiv);
 
         const buttonHeadingDiv = document.createElement('div');
-        buttonHeadingDiv.className = 'show-heading__button';
+        buttonHeadingDiv.className = 'show-headings__button';
         buttonHeadingDiv.textContent = '';
         headingsRowDiv.appendChild(buttonHeadingDiv);
 
@@ -113,7 +113,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // BUTTON Section
             const buttonSectionDiv = document.createElement('div');
-            buttonSectionDiv.className = 'show__section';
+            buttonSectionDiv.className = 'show__section show__section-button';
             showDiv.appendChild(buttonSectionDiv);
 
             const buttonTextDiv = document.createElement('div');
@@ -138,7 +138,7 @@ document.addEventListener('DOMContentLoaded', () => {
             showsContainer.appendChild(showDiv);
             console.log('showDiv appended to showsContainer');
 
-            // Add a divideer after each show           
+            // Add a divider after each show           
             const dividerDiv = document.createElement('div');
             dividerDiv.className = 'show-divider'
             showsContainer.appendChild(dividerDiv);
@@ -155,98 +155,4 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Resize event listener added');
 
 });
-    // Function to create Heading row (for tablet and desktop view)
-    // function createHeadingsRow() {
-    //     console.log('Creating heading row...');
-    //     const headingRowDiv = document.createElement('div');
-    //     headingRowDiv.className = 'show-heading';
-    // };
-
-//     const renderShows() {
-//         showsContainer.innerHTML = '';
-
-//         // loop through each show and create its corresponding elements
-
-//         shows.forEach((show, index) => {
-
-//             const showDiv = document.createElement('div');
-//             showDiv.className = 'show';
-
-//             const dateSection = document.createElement('div');
-//             dateSection.className = 'show__section';
-//             showDiv.appendChild(dateSection);
-        
-//             const dateLabelDiv = document.createElement('div');
-//             dateLabelDiv.className = 'show__label';
-//             dateLabelDiv.textContent = 'DATE'
-//             showDiv.appendChild(dateLabelDiv);
-
-//             const dateSpan = document.createElement('span');
-//             dateSpan.className = 'show__date show-item--flex-row';
-//             dateSpan.textContent = show.date;
-//             showDiv.appendChild(dateSpan);
-
-//             const venueSection = document.createElement('div');
-//             venueSection.className = 'show__section';
-//             showDiv.appendChild(venueSection);
-
-//             const venueLabelDiv = document.createElement('div');
-//             venueLabelDiv.className = 'show__label';
-//             venueLabelDiv.textContent = 'VENUE'
-//             showDiv.appendChild(venueLabelDiv);
-
-//             const venueSpan = document.createElement('span');
-//             venueSpan.className = 'show__venue show-item--flex-row';
-//             venueSpan.textContent = show.venue;
-//             showDiv.appendChild(venueSpan);
-
-//             const locationSection = document.createElement('div');
-//             locationSection.className = 'show__section';
-//             showDiv.appendChild(locationSection);
-
-//             const locationLabelDiv = document.createElement('div');
-//             locationLabelDiv.className = 'show__label';
-//             locationLabelDiv.textContent = 'LOCATION';
-//             showDiv.appendChild(locationLabelDiv);
-
-//             const locationSpan = document.createElement('span');
-//             locationSpan.className = 'show__location show-item--flex-row';
-//             locationSpan.textContent = show.location;
-//             showDiv.appendChild(locationSpan);
-
-//             // Button : Buy Tickets
-//             const buttonDiv = document.createElement('div')
-//             buttonDiv.className = 'show__button-section';
-//             showDiv.appendChild(buttonDiv);
-
-//             const buyTicketsButton = document.createElement('button');
-//             buyTicketsButton.className = 'show__button show-item--flex-row'
-//             buyTicketsButton.textContent = 'BUY TICKETS';
-//             buttonDiv.appendChild(buyTicketsButton);
-
-            
-
-
-//             // Creating Click eventListener
-//             showDiv.addEventListener('click', () => {
-//                 const selectedShow = document.querySelector('.show-selected');
-
-//                 if (selectedShow) {
-//                     selectedShow.classList.remove('show-selected');
-//                 }
-
-//                 showDiv.classList.add('show-selected')
-//             });
-
-//             showsContainer.appendChild(showDiv);    
-            
-//             // adding divider after each show 
-//             const dividerDiv = document.createElement('div');
-//             dividerDiv.className = 'show-divider';
-//             showsContainer.appendChild(dividerDiv);
-           
-//         });
-//     }
-
-//     renderShows();
-// })
+    
